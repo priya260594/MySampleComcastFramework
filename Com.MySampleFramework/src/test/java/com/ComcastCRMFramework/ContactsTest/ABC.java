15efef6f64295289296936cb8d0a65efef9a53df
User <User@DESKTOP-6P64TEG>
2024-10-01 12:43:29 +0530

    framework created

---
+++ Com.MySampleFramework/src/test/java/com/ComcastCRMFramework/ContactsTest/ABC.java
@@ -0,0 +1,16 @@
+package com.ComcastCRMFramework.ContactsTest;
+
+import org.testng.annotations.Test;
+
+public class ABC {
+	@Test
+	public void contactCreation() {
+		
+		System.out.println(" i created abc");
+		System.out.println(" i created abcgde");
+		System.out.println(" i created abcgdefrdfv");
+		
+	}
+	
+
+}
---
+++ Com.MySampleFramework/src/test/java/com/ComcastCRMFramework/ContactsTest/ContactsTest.java
@@ -0,0 +1,11 @@
+package com.ComcastCRMFramework.ContactsTest;
+
+import org.testng.annotations.Test;
+
+public class ContactsTest {
+	@Test
+	public void SampleTest() {
+		System.out.println("initial");
+	}
+
+}
