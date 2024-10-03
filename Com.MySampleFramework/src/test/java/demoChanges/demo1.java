856813dedaa29e9a3104352c00ffae989b9ce636
Admin <Admin@Jyoti>
2024-10-03 17:28:35 +0530

    first added is done

---
+++ Com.MySampleFramework/src/test/java/demoChanges/demo1.java
@@ -0,0 +1,10 @@
+package demoChanges;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("TestScript is excuted ,and updation is worked");
+	}
+
+}
